c72de863f3417e899f3dca3d9fab14037fd14803
muhammadwasil4455 <wasiltanoli37@gmail.com>
2025-02-13 10:53:36 +0500

    assign

---
+++ app.js
@@ -0,0 +1,10 @@
+// let arr=["karachi","lahore","islamabad","quetta"];
+
+// let nums=[3,7,12];
+// console.log("nums")
+
+let nums = [3, 7, 12];
+let num2 = [nums[2], nums[0], nums[1]];
+
+console.log(num2);
+
